938d573c93ad7bf47ec015cc646e809abe4a49a5
DagmarTimmreck <DagmarTimmreck@users.noreply.github.com>
2017-10-27 16:49:24 +0200

    fix connection to UI

--- server.js
+++ server.js
@@ -1,20 +1,26 @@
 const Express = require('express');
+const cors = require('cors');
 const bodyParser = require('body-parser-json');
 const morgan = require('morgan');
-const sqlite3 = require('sqlite3');
 
 const app = new Express();
 module.exports = app;
 
-const PORT = process.env.PORT || 4001;
+const PORT = process.env.PORT || 4000;
 
-const db = new sqlite3.Database(process.env.TEST_DATABASE || './database.sqlite');
+// use cors for cross origin requests
+app.use(cors());
 
 // automatically parse json body to req.body
 app.use(bodyParser.json());
 
 // logging with dev infos
 app.use(morgan('dev'));
+
+// Mount apiRouter below at the '/api' path.
+const apiRouter = require('./routers/api.js');
+
+app.use('/api', apiRouter);
 
 // serve static content from folder 'public'
 app.use(Express.static('public'));
